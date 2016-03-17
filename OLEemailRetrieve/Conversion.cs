151e9b546a960b0a7eed559aa89d48e4f9aac089
Shakey <williedevonsmith@gmail.com>
2016-03-17 15:38:32 -0500

    Initial Commit

---
+++ OLEemailRetrieve/Conversion.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace OLEemailRetrieve
+{
+    class Conversion
+    {
+
+        public void ConvertXls(string fileName)
+        { 
+            DataTable dt = new DataTable();
+            InternalEntities db = new InternalEntities();
+
+            using (SpreadsheetDocument spreadSheetDocument = SpreadsheetDocument.Open(fileName, false))
+            {
+
+                WorkbookPart workbookPart = spreadSheetDocument.WorkbookPart;
+                IEnumerable<Sheet> sheets = spreadSheetDocument.WorkbookPart.Workbook.GetFirstChild<Sheets>().Elements<Sheet>();
+                string relationshipId = sheets.First().Id.Value;
+                WorksheetPart worksheetPart = (WorksheetPart)spreadSheetDocument.WorkbookPart.GetPartById(relationshipId);
+                Worksheet workSheet = worksheetPart.Worksheet;
+                SheetData sheetData = workSheet.GetFirstChild<SheetData>();
+                IEnumerable<Row> rows = sheetData.Descendants<Row>();
+
+                foreach (Cell cell in rows.ElementAt(0))
+                {
+                    dt.Columns.Add(GetCellValue(spreadSheetDocument, cell));
+                }
+
+                foreach (Row row in rows) // Include the header row
+                {
+                    DataRow tempRow = dt.NewRow();
+
+                    for (int i = 0; i < row.Descendants<Cell>().Count(); i++)
+                    {
+                        tempRow[i] = GetCellValue(spreadSheetDocument, row.Descendants<Cell>().ElementAt(i));
+                    }
+
+                    dt.Rows.Add(tempRow);
+                }
+
+            }
+            dt.Rows.RemoveAt(0); // Remove header row.
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                DataRow dr = dt.Rows[i];
+                var RequestId = Convert.ToInt32(dr["Request Id"]);
+                var Response = dr["Response"].ToString();
+                var GermanyResponder = dr["Germany Responder"].ToString();
+                var resp = db.OnlineExpedites.Where(x => x.RequestId == RequestId).FirstOrDefault<OnlineExpedite>();
+
+                if (resp != null)
+                {
+                    resp.Response = Response;
+                }
+
+                using (var dbCtx = new InternalEntities())
+                {
+                    dbCtx.Entry(resp).State = System.Data.Entity.EntityState.Modified;
+
+                    dbCtx.SaveChanges();
+                }
+            }
+        }
+
+        public static string GetCellValue(SpreadsheetDocument document, Cell cell)
+        {
+            SharedStringTablePart stringTablePart = document.WorkbookPart.SharedStringTablePart;
+            string value = cell.CellValue.InnerXml;
+
+            if (cell.DataType != null && cell.DataType.Value == CellValues.SharedString)
+            {
+                return stringTablePart.SharedStringTable.ChildElements[Int32.Parse(value)].InnerText;
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+    }
+}
---
+++ OLEemailRetrieve/Program.cs
@@ -0,0 +1,177 @@
+﻿using Microsoft.Exchange.WebServices.Data;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OLEemailRetrieve
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conversion ssConversion = new Conversion();
+            
+            string dt = DateTime.Now.ToString("MMddyyyyhhmmss");
+            string path = @"ImportedFiles\";
+            string log = @"Logs\";
+            string logfilename = dt + ".txt";
+
+            try
+            {
+                if (!Directory.Exists(log))
+                {
+                    Directory.CreateDirectory(log);
+                }
+                //streamwriter.WriteLine("Logs directory created!");
+            }
+
+            catch (Exception ex)
+            {
+                //streamwriter.WriteLine(ex.ToString());
+            }
+
+            FileStream filestream = new FileStream(log + logfilename, FileMode.Create);
+            var streamwriter = new StreamWriter(filestream);
+            streamwriter.AutoFlush = true;
+            Console.SetOut(streamwriter);
+            Console.SetError(streamwriter);
+            
+            try
+            {
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                streamwriter.WriteLine("ImportedFiles directory created!");
+            }
+            catch (Exception ex)
+            {
+                streamwriter.WriteLine(ex.ToString());
+            }
+
+            string mEmailTo = ConfigurationManager.AppSettings["EmailTo"];
+            string mEmailFrom = ConfigurationManager.AppSettings["EmailFrom"];
+            string mEmailSubject = ConfigurationManager.AppSettings["EmailSubject"];
+            
+            ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2007_SP1);
+
+            if (ConfigurationManager.AppSettings["UseDefault"] != "true")
+            {
+                service.Credentials = new WebCredentials(ConfigurationManager.AppSettings["User"], ConfigurationManager.AppSettings["Password"], ConfigurationManager.AppSettings["Domain"]);
+                service.TraceEnabled = true;
+                service.TraceFlags = TraceFlags.All;
+
+                try
+                {
+                    service.AutodiscoverUrl(ConfigurationManager.AppSettings["Address"], RedirectionUrlValidationCallback);
+                }
+                catch
+                {
+                    streamwriter.WriteLine("Not a valid [Address] in app config");
+                }
+                
+            }
+            else
+            {
+                service.UseDefaultCredentials = true;
+                try
+                {
+                    service.AutodiscoverUrl(mEmailFrom, RedirectionUrlValidationCallback);
+                }
+                catch
+                {
+                    streamwriter.WriteLine("Not a valid [EMAILFROM] in app config");
+                }
+                
+            }
+
+            // Add a search filter that searches on the body or subject.
+            List<SearchFilter> searchFilterCollection = new List<SearchFilter>();
+            searchFilterCollection.Add(new SearchFilter.ContainsSubstring(ItemSchema.Subject, mEmailSubject));
+            SearchFilter searchFilter = new SearchFilter.SearchFilterCollection(LogicalOperator.Or, searchFilterCollection.ToArray());
+
+            // Create a view with a page size of 10.
+            ItemView view = new ItemView(10);
+
+            // Identify the Subject and DateTimeReceived properties to return.
+            // Indicate that the base property will be the item identifier
+            view.PropertySet = new PropertySet(BasePropertySet.IdOnly, ItemSchema.Subject);
+            
+            // Order the search results by the DateTimeReceived in descending order.
+            view.OrderBy.Add(ItemSchema.DateTimeReceived, SortDirection.Descending);
+            
+            view.Traversal = ItemTraversal.Shallow;
+
+            FindItemsResults<Item> findResults = service.FindItems(WellKnownFolderName.Inbox, searchFilter, view);
+
+            foreach (Item item in findResults.Items)
+            {
+                // Bind to an existing message item, requesting its Id property plus its attachments collection.
+                EmailMessage message = EmailMessage.Bind(service, new ItemId(item.Id.ToString()), new PropertySet(BasePropertySet.IdOnly, ItemSchema.Attachments));
+
+                // Iterate through the attachments collection and load each attachment.
+                if (message.Attachments.Count == 1)
+                {
+                    foreach (Attachment attachment in message.Attachments)
+                    {
+                        FileAttachment fileAttachment = attachment as FileAttachment;
+
+                        // Load the file attachment into memory and print out its file name.
+                        fileAttachment.Load();
+                        streamwriter.WriteLine("Attachment name: " + fileAttachment.Name);
+
+                        // Load attachment contents into a file.
+                        fileAttachment.Load(path + fileAttachment.Name);
+
+                        string fileName = path + fileAttachment.Name;
+                        ssConversion.ConvertXls(fileName);
+                    }
+                }
+                else
+                {
+                    foreach (Attachment attachment in message.Attachments)
+                    {
+
+                        if (attachment is FileAttachment && attachment.ContentType == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+                        {
+                            FileAttachment fileAttachment = attachment as FileAttachment;
+
+                            // Load the file attachment into memory and print out its file name.
+                            fileAttachment.Load();
+                            streamwriter.WriteLine("Attachment name: " + fileAttachment.Name);
+
+                            // Load attachment contents into a file.
+                            fileAttachment.Load(path + fileAttachment.Name);
+
+                            string fileName = path + fileAttachment.Name;
+                            ssConversion.ConvertXls(fileName);
+                        }
+                    }
+                }
+                //streamwriter.WriteLine(item.Subject + "\n" + item.Id);
+            }    
+        }
+        private static bool RedirectionUrlValidationCallback(string redirectionUrl)
+        {
+            // The default for the validation callback is to reject the URL.
+            bool result = false;
+
+            Uri redirectionUri = new Uri(redirectionUrl);
+
+            // Validate the contents of the redirection URL. In this simple validation
+            // callback, the redirection URL is considered valid if it is using HTTPS
+            // to encrypt the authentication credentials. 
+            if (redirectionUri.Scheme == "https")
+            {
+                result = true;
+            }
+            return result;
+        }
+    }
+}
